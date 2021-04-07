3208214b223f0796e3d8b6679bf409aa9210cbc9
Toomas Arumae <tom.arumae@gmail.com>
2021-04-07 15:11:03 +0100

    Reverted from useHook to old style

--- src/App.js
+++ src/App.js
@@ -1,20 +1,20 @@
-import React, { useState } from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import Person from './Person/Person.js';
 
-const app = props => {
-  const [peopleState, setPeopleState] = useState({
+class App extends Component {
+
+  state = {
     people: [
       { name: 'Tom', age: 28 },
       { name: 'Alex', age: 35 },
       { name: 'Sam', age: 21 }
-    ]
-  });
+    ],
+    otherState: 'some other value'
+  }
 
-  const [otherState, setOtherState] = useState({ otherState: 'some other value' });
-
-  const switchNameHandler = (newName) => {
-    setPeopleState({
+  switchNameHandler = (newName) => {
+    this.setState({
       people: [
         { name: newName, age: 28 },
         { name: 'Alex', age: 35 },
@@ -22,12 +22,9 @@ const app = props => {
       ]
     });
   }
-   // Two ways of passing down functions. 
-   // First one is on the button with passing an anonymous arrow function.
-   // Second one is binding the function with this, and passing the argument as a second parameter.
   
-  const nameChangedHandler = (event) => {
-    setPeopleState({
+  nameChangedHandler = (event) => {
+    this.setState({
       people: [
         { name: 'Tom', age: 28 },
         { name: event.target.value, age: 35 },
@@ -36,24 +33,31 @@ const app = props => {
     })
   }
   
-  const style = {
-    backgroundColor: 'white',
-    font: 'inherit',
-    border: '1px solid blue',
-    padding: '8px',
-    cursor: 'pointer'
-  };
+  
+  // Two ways of passing down functions. 
+  // First one is on the button with passing an anonymous arrow function.
+  // Second one is binding the function with this, and passing the argument as a second parameter.
+  
+  render () {
+    const style = {
+      backgroundColor: 'white',
+      font: 'inherit',
+      border: '1px solid blue',
+      padding: '8px',
+      cursor: 'pointer'
+    };
 
-  return (
-    <div className="App">
-      <h1>Hi, I'm a react App.</h1>
-      <p>This is really working!</p>
-      <button style={style} onClick={() => switchNameHandler("Toomas")}>Switch Name</button>
-      <Person name={peopleState.people[0].name} age={peopleState.people[0].age} click={switchNameHandler.bind(this, 'Tom!')} />
-      <Person name={peopleState.people[1].name} age={peopleState.people[1].age} changed={nameChangedHandler}/>
-      <Person name={peopleState.people[2].name} age={peopleState.people[2].age}> My hobbies: cartoons </Person>
-    </div>
-  );
+    return (
+      <div className="App">
+        <h1>Hi, I'm a react App.</h1>
+        <p>This is really working!</p>
+        <button style={style} onClick={() => this.switchNameHandler("Toomas")}>Switch Name</button>
+        <Person name={this.state.people[0].name} age={this.state.people[0].age} click={this.switchNameHandler.bind(this, 'Tom!')} />
+        <Person name={this.state.people[1].name} age={this.state.people[1].age} changed={this.nameChangedHandler}/>
+        <Person name={this.state.people[2].name} age={this.state.people[2].age}> My hobbies: cartoons </Person>
+      </div>
+    );
+  }
 }
 
-export default app;
+export default App;
